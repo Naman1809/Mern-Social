356d95371d73117122f56800772970af7fc33ba6
Naman1809 <namankukreja01@gmail.com>
2023-09-08 10:57:21 +0530

    completed till update password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,11 +2,11 @@ const User = require("../models/User");
 const Post = require("../models/Post");
 const {sendEmail}=require("../middlewares/sendEmail")
 const crypto =  require("crypto");
-
+const cloudinary = require("cloudinary")
 
 exports.register = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, email, password, avatar } = req.body;
     let user = await User.findOne({ email });
     if (user) {
       return res
@@ -14,11 +14,16 @@ exports.register = async (req, res) => {
         .json({ success: false, message: "User already exists" });
     }
 
+    const myCloud = await cloudinary.v2.uploader.upload(avatar,{
+      folder: "avatars"
+    });
+
+
     user = await User.create({
       name,
       email,
       password,
-      avatar: { public_id: "sample_id", url: "sampleurl" },
+      avatar: { public_id: myCloud.public_id, url: myCloud.secure_url },
     });
 
     const token = await user.generateToken();
@@ -173,13 +178,21 @@ exports.updatePassword = async (req, res) => {
 exports.updateProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user._id);
-    const { name, email } = req.body;
+    const { name, email,avatar } = req.body;
 
     if (name) {
       user.name = name;
     }
     if (email) {
       user.email = email;
+    }
+    if(avatar){
+      await cloudinary.v2.uploader.destroy(user.avatar.public_id)
+      const myCloud = await cloudinary.v2.uploader.upload(avatar,{
+        folder:"avatars",
+      })
+      user.avatar.public_id = myCloud.public_id;
+      user.avatar.url = myCloud.secure_url;
     }
     await user.save();
 
